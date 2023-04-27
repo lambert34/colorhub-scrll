fe2fa1d791808bf5e3dc23cebbf144eb8a40c410
lambert34 <aleksandrlambert@gmail.com>
2023-04-27 21:09:18 +0700

    добавляет условие не isTouch
    чтобы скролл переставал работать на мобилке и проч устройствах

--- Color-hub-scroll/project/js/app.js
+++ Color-hub-scroll/project/js/app.js
@@ -1,8 +1,10 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
-ScrollSmoother.create({
-    wrapper: '.wrapper',
-    content: '.content',
-    smooth: 1.5,
-    effects: true 
-})
+if (ScrollTrigger.isTouch !==1) {
+    ScrollSmoother.create({
+        wrapper: '.wrapper',
+        content: '.content',
+        smooth: 1.5,
+        effects: true 
+    })
+}
