5a6035cd76f498fb6f9d4e26e47520d70ff4629e
lambert34 <aleksandrlambert@gmail.com>
2023-04-27 22:06:52 +0700

    добавляет код для анимации при скролле

--- Color-hub-scroll/project/js/app.js
+++ Color-hub-scroll/project/js/app.js
@@ -18,12 +18,31 @@ gsap.fromTo('.hero-section', { opacity: 1}, {
     }
 })
 
-gsap.fromTo('.gallery__left .gallery__item', { opacity: 0 }, {
-    opacity: 1,
-    scrollTrigger: {
-        trigger: '.gallery__item',
-        scrub: true
-    }
+let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
+
+itemsL.forEach(item => {
+    gsap.fromTo(item, { x: -500, opacity: 0 }, {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+            trigger: item,
+            start: '-850',
+            end: '-100',
+            scrub: true
+        }
+    })
 })
 
+let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+
+itemsR.forEach(item => {
+    gsap.fromTo(item, { x: 500, opacity: 0 }, {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+            trigger: item,
+            start: '-850',
+            end: '-100',
+            scrub: true
+        }
+    })
+})
 }
